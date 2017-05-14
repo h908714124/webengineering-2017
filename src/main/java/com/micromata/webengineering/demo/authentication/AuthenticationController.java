ddf8bb0e11d028555a1d2edf39e0bd358bd6568c
h908714124 <kraftdurchblumen@gmx.de>
2017-05-14 22:17:58 +0200

    auch cookie setzen

--- src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationController.java
+++ src/main/java/com/micromata/webengineering/demo/authentication/AuthenticationController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/user")
 public class AuthenticationController {
@@ -20,12 +23,17 @@ public class AuthenticationController {
     private AuthenticationService service;
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public ResponseEntity<AuthenticationService.UserToken> login(@RequestBody UserLogin userLogin) {
+    public ResponseEntity<AuthenticationService.UserToken> login(
+            HttpServletResponse response,
+            @RequestBody UserLogin userLogin) {
         AuthenticationService.UserToken token = service.login(userLogin.email, userLogin.password);
-
         if (token == null) {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
+        Cookie cookie = new Cookie("access_token", token.token);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(-1);
+        response.addCookie(cookie);
         return new ResponseEntity<>(token, HttpStatus.OK);
     }
 }
